ebb95b363f5c3f8cf241be4ac4e879e7a83ffd07
ousttrue <ousttrue@gmail.com>
2019-11-03 06:52:39 +0900

    natvis

--- source/app.d
+++ source/app.d
@@ -1,6 +1,42 @@
 import std.stdio;
 
+struct SliceView(T)
+{
+	T* ptr;
+	ulong size;
+
+	this(T[] src)
+	{
+		ptr = src.ptr;
+		size = src.length;
+	}
+}
+
+struct Point
+{
+	float x;
+	float y;
+}
+
+SliceView!T makeView(T)(T[] slice)
+{
+	return SliceView!T(slice);
+}
+
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	auto array = [1, 2, 3];
+	auto slice = makeView(array);
+
+	auto text = "Edit source/app.d to start your project.";
+	auto textSlice = makeView(text);
+
+	Point[] points = [
+		{1, 2},
+		{3, 4},
+	];
+	auto pointSlice = makeView(points);
+
+	writeln(text);
+	writeln(string.stringof);
 }
